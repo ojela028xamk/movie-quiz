ee45680b852e3a7f5b84343be63faf4358b57bcc
ojela028xamk <jerelans91@gmail.com>
2024-03-02 14:53:02 +0200

    prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import css from './App.module.scss';
+import css from './App.module.scss'
 
 const App = (): JSX.Element => {
   return (
     <div className={css.App}>
       <h2>Movie Quiz</h2>
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
