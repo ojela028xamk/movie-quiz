bc0ebfa778c7059d59d03ef86bca41cb39b30eef
ojela028xamk <jerelans91@gmail.com>
2024-03-11 12:48:29 +0200

    results component

---
+++ src/MovieQuiz/MovieQuizResults.tsx
@@ -0,0 +1,11 @@
+import css from './MovieQuiz.module.scss'
+
+const MovieQuizResults = (): JSX.Element => {
+  return (
+    <div className={css.movie_quiz_results}>
+      <h2>Results</h2>
+    </div>
+  )
+}
+
+export default MovieQuizResults
