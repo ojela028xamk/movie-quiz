891b13377aefb38ae1fd43ad7a37319e183710b3
ojela028xamk <jerelans91@gmail.com>
2024-04-03 15:58:13 +0300

    useless import

--- src/MovieQuiz/MovieQuizContainer.tsx
+++ src/MovieQuiz/MovieQuizContainer.tsx
@@ -1,4 +1,4 @@
-import { Button, Spinner } from 'react-bootstrap'
+import { Button } from 'react-bootstrap'
 import { MovieImageItem, MovieResult, QuizQuestion } from '../globalTypes'
 import MovieQuiz from './MovieQuiz'
 import { useEffectOnce } from 'react-use'
